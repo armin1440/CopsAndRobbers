a5fc099d8352fead9eb9943eb0085fe053fb73d6
Armin1440 <armin1440@gmail.com>
2020-01-31 20:15:10 +0330

    added everything

---
+++ cmake-build-debug/CMakeFiles/CopsAndRobbers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CopsAndRobbers.dir/CopsAndRobbers.c.obj"
+  "CopsAndRobbers.exe"
+  "CopsAndRobbers.exe.manifest"
+  "CopsAndRobbers.pdb"
+  "libCopsAndRobbers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CopsAndRobbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
